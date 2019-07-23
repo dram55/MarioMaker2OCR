f477e425adf3cbbe6f231b2448945be9e61172af
dram55 <isbister1500@hotmail.com>
2019-07-22 20:06:38 -0400

    drambar now handles level clear condition.

--- MarioMaker2OCR/web/drambar/drambar.js
+++ MarioMaker2OCR/web/drambar/drambar.js
@@ -1,123 +1,136 @@
-(function(){
-   'use strict';
+(function () {
+      'use strict';
 
-   var app = angular.module('dramBarApp',['angular-websocket']);
-   app.controller('dramBarController',dramBarController);
-   app.factory('smmFactory', smmFactory);
+      var app = angular.module('dramBarApp', ['angular-websocket']);
+      app.controller('dramBarController', dramBarController);
+      app.factory('dramBarFactory', dramBarFactory);
 
-   dramBarController.inject = ['smmFactory', 'dramBarSettings'];
-   function dramBarController(smmFactory, dramBarSettings) {
-      var ctrl = this;
-      ctrl.smm = smmFactory;
-      ctrl.settings = dramBarSettings;
-   }
+      dramBarController.inject = ['dramBarFactory', 'dramBarSettings'];
 
-   smmFactory.inject = ['$websocket', '$timeout', '$interval', 'dramBarSettings'];
-   function smmFactory($websocket, $timeout, $interval, dramBarSettings) {
-      var server = 'ws://localhost:3000/wss';
-      var socket = $websocket(server,null,{reconnectIfNotNormalClose: true});
-
-      var levelStartTime;
-
-      var status = {
-         deathCount:0,
-         changeInProgress:true,
-         level: {},
-         levelTimer: "",
-         deathInProgress:false
+      function dramBarController(dramBarFactory, dramBarSettings) {
+         var ctrl = this;
+         ctrl.state = dramBarFactory;
+         ctrl.settings = dramBarSettings;
       }
 
-      // Timer functions - time on current level
-      function startTimer(){
-         levelStartTime = new Date();
+      dramBarFactory.inject = ['$websocket', '$timeout', '$interval', 'dramBarSettings'];
+
+      function dramBarFactory($websocket, $timeout, $interval, dramBarSettings) {
+         var server = 'ws://localhost:3000/wss';
+         var socket = $websocket(server, null, {reconnectIfNotNormalClose: true});
+
+         var levelStartTime;
+         var timerRunning;
+
+         var state = {
+            deathCount: 0,
+            hideCurrentLevel: true,
+            level: {},
+            levelTimer: "",
+            deathInProgress: false,
+            levelCleared: false
+         };
+
+         // Timer functions - time on current level
+         function startTimer() {
+            levelStartTime = new Date();
+            timerRunning = true;
+         }
+
+         function pauseTimer() {
+            timerRunning = false;
+         }
+
+         function timerTick() {
+            if (!timerRunning) {
+               return;
+            }
+            var currentTime = new Date().getTime();
+            var totalSeconds = (currentTime - levelStartTime) / 1000;
+            var hours = Math.floor(totalSeconds / 3600);
+            var minutes = (Math.floor(totalSeconds / 60) % 60);
+            var seconds = Math.floor(totalSeconds % 60);
+            state.levelTimer = "";
+            if (hours > 0) state.levelTimer += hours.toString().padStart(2, '0') + ":";
+            state.levelTimer += minutes.toString().padStart(2, '0');
+            state.levelTimer += ":" + seconds.toString().padStart(2, '0');
+            if (dramBarSettings.playTimerWarning && (dramBarSettings.playTimerWarningAt * 60) == Math.floor(totalSeconds)) {
+               document.getElementById("timerAudio").play();
+            }
+         }
          $interval(timerTick, 1000);
-      }
-      function pauseTimer(){
-         $interval.cancel(stop);
-      }
-      function resetTimer(){
-         $interval.cancel(stop);
-         status.levelTimer = "";
-      }
-      function timerTick(){
-         var currentTime = new Date().getTime();
-         var totalSeconds = (currentTime - levelStartTime) / 1000;
-         var hours = Math.floor(totalSeconds / 3600);
-         var minutes = (Math.floor(totalSeconds / 60) % 60);
-         var seconds = Math.floor(totalSeconds % 60);
-         status.levelTimer = "";
-         if (hours>0) status.levelTimer += hours.toString().padStart(2, '0') + ":";
-         status.levelTimer += minutes.toString().padStart(2, '0');
-         status.levelTimer += ":" + seconds.toString().padStart(2, '0');
-         if(dramBarSettings.playTimerWarning && (dramBarSettings.playTimerWarningAt * 60) == Math.floor(totalSeconds)) {
-            document.getElementById("timerAudio").play();
+
+         socket.onMessage(function (message) {
+            var event = JSON.parse(message.data);
+
+            switch (event.type) {
+               case 'death': death(event); break;
+               case 'restart': restart(event); break;
+               case 'exit': exit(event); break;
+               case 'clear': clear(event); break;
+               case 'level': newLevel(event); break;
+            }
+
+            if (event.level) newLevel(event);
+         });
+
+         function death() {
+            console.log('death');
+            if (!state.deathInProgress) {
+               state.deathInProgress = true;
+               $timeout(function () {
+                  state.deathCount = state.deathCount + 1;
+                  state.deathInProgress = false;
+               }, 500);
+            }
          }
-      }
-      $interval(timerTick, 1000);
 
-      socket.onMessage(function(message) {
-         var event = JSON.parse(message.data);
-
-         switch(event.type) {
-            case 'death':   death(event); break;
-            case 'restart': death(event); break;
-            case 'exit':    exit(event);    break;
-            case 'clear':   clear(event);   break;
-            case 'level':   newLevel(event);   break;
-          }
-
-          if (event.level) newLevel(event);
-      });
-
-      function death() {
-         console.log('death');
-         if (!status.deathInProgress) {
-            status.deathInProgress = true;
-            $timeout(function() {
-               status.deathCount = status.deathCount + 1;
-               status.deathInProgress = false;
-            }, 500);
+         function restart() {
+            // if you are starting over after clear - then refresh timer
+            if (state.levelCleared) {
+               displayNewLevel(state.level);
+            } else {
+               death();
+            }
          }
-       }
-       function restart(){
-         console.log('restart');
-         status.deathCount = status.deathCount + 1;
-       }
-       function exit(){
-         console.log('exit');
-         status.changeInProgress = true;
-         status.deathCount = 0;
-         pauseTimer();
-       }
-       function clear(data){
-         console.log('clear');
-         pauseTimer();
-       }
 
-       function newLevel(data){
-         if (data.level.code !== status.level.code) {
+         function exit() {
+            console.log('exit');
+            pauseTimer();
+
+            state.hideCurrentLevel = true;
+         }
+
+         function clear(data) {
+            console.log('clear');
+            pauseTimer();
+            state.levelCleared = true;
+         }
+
+         function newLevel(data) {
+            // Only process new level if the level data is hidden OR if the level code has changed.
+            if (data.level.code !== state.level.code || state.hideCurrentLevel) {
+               // If panel is hidden, display immediately
+               if (state.hideCurrentLevel) {
+                  displayNewLevel(data.level);
+               } else {
+                  // delay 1.5 seconds - to allow for scroll animation
+                  state.hideCurrentLevel = true;
+                  $timeout(function () {
+                     displayNewLevel(data.level);
+                  }, 1500);
+               }
+            }
+         }
+
+         function displayNewLevel(newLevel) {
+            state.hideCurrentLevel = false;
+            state.levelCleared = false;
+            state.level = newLevel;
+            state.deathCount = 0;
+
             startTimer();
-            if (status.changeInProgress) {
-               displayNewLevel();
-            }
-            else {
-               // delay 2 seconds - for animation to remove current level
-               status.changeInProgress = true;
-               $timeout(function() {
-                  displayNewLevel();
-               }, 1500);
-            }
          }
-
-         function displayNewLevel(){
-            status.changeInProgress = false;
-            status.level = data.level;
-            status.deathCount = 0;
-            levelStartTime = new Date().getTime();
-         }
-       }
-
-      return status;
-   }
-
+         return state;
+      }
 })();
