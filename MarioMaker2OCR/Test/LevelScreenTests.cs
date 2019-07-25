8a930b74b902226aab80463d0818e2332742d0a3
dram55 <isbister1500@hotmail.com>
2019-07-24 21:45:21 -0400

    Add LevelScreenTests

---
+++ MarioMaker2OCR/Test/LevelScreenTests.cs
@@ -0,0 +1,40 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using MarioMaker2OCR.Objects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace MarioMaker2OCR.Test
+{
+    [TestClass]
+    public class LevelScreenTests
+    {
+        [TestMethod]
+        public void ReadCorrectLevelCode()
+        {
+            string[] filePaths = Directory.GetFiles("./Test/testdata/frames/720/levels", "*.png", SearchOption.TopDirectoryOnly);
+
+            foreach (var file in filePaths)
+            {
+                int levelIdx = file.LastIndexOf("level.") + 6;
+                string actualLevelCode = file.Substring(levelIdx, 11);
+
+                using (var testFrame = new Image<Bgr, byte>(file))
+                {
+                    Level level = OCRLibrary.GetLevelFromFrame(testFrame);
+                    Assert.IsTrue(level.code == actualLevelCode, $"Level code OCR ({level.code}) does not match actual code ({actualLevelCode})");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void DetectLevelScreen()
+        {
+
+        }
+    }
+}
