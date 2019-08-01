8f76f5b07a1d526a309e6ad9aff93d1f7afdcd54
dram55 <isbister1500@hotmail.com>
2019-08-01 08:33:46 -0400

    More customization options to drambar in settings.js

--- MarioMaker2OCR/web/drambar/settings.js
+++ MarioMaker2OCR/web/drambar/settings.js
@@ -5,6 +5,9 @@
    app.constant('dramBarSettings', {
       isTimerVisible:true, 
       isDeathCountVisible:true,
+      isLevelNameVisible:true,
+      isLevelAuthorVisible:true,
+      isLevelCodeVisible:true,
       slideFromDirection:'left', //left|right|top|bottom
       playTimerWarning:false,
       playTimerWarningAt:15, //minutes
