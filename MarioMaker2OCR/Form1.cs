faf85264782f5f09944d2a324eac26e2fdd5dbc3
dram55 <isbister1500@hotmail.com>
2019-08-02 08:53:01 -0400

    Expand ROI on clearscreen for startover and quit - supports comments on screen

--- MarioMaker2OCR/Form1.cs
+++ MarioMaker2OCR/Form1.cs
@@ -30,7 +30,7 @@ namespace MarioMaker2OCR
         private EventTemplate[] engTemplates = new EventTemplate[] {
             new EventTemplate("./templates/480/exit.png", "exit", 0.8, new Rectangle[] {
                 new Rectangle(new Point(400,330), new Size(230, 65)), //Pause Menu
-                new Rectangle(new Point(410,325), new Size(215, 60)), //Clear Screen
+                new Rectangle(new Point(410,225), new Size(215, 160)), //Clear Screen
             }),
             new EventTemplate("./templates/480/quit.png", "exit", 0.9, new Rectangle[] {
                 new Rectangle(new Point(408,338), new Size(224, 70)) //Pause Menu
@@ -40,7 +40,7 @@ namespace MarioMaker2OCR
             }),
             new EventTemplate("./templates/480/startover.png", "restart", 0.8, new Rectangle[] {
                 new Rectangle(new Point(400,275), new Size(230, 65)), //Pause Menu
-                new Rectangle(new Point(195,325), new Size(215, 60)), //Clear Screen
+                new Rectangle(new Point(195,225), new Size(215, 160)), //Clear Screen
             }),
             new EventTemplate("./templates/480/death_big.png", "death", 0.8),
             new EventTemplate("./templates/480/death_small.png", "death", 0.8),
