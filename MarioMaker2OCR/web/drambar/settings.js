87a1bd15f736f173e3c7aa0be8484636e16cb27a
dram55 <isbister1500@hotmail.com>
2019-08-05 08:11:27 -0400

    Customizable color font-size and color for drambar in settings.js

--- MarioMaker2OCR/web/drambar/settings.js
+++ MarioMaker2OCR/web/drambar/settings.js
@@ -3,14 +3,25 @@
 
    var app = angular.module('dramBarApp');
    app.constant('dramBarSettings', {
+
+      // Which Components to see?
       isTimerVisible:true, 
       isDeathCountVisible:true,
       isLevelNameVisible:true,
       isLevelAuthorVisible:true,
       isLevelCodeVisible:true,
+
+
+      // How do they look?
       slideFromDirection:'left', //left|right|top|bottom
+      levelCodeColor: '#ff49ad',
+      levelNameColor: '#f5f5f5',
+      levelAuthorColor: '#f5f5f5',
+      fontSize: '100%',
+      
+
+      //Other options
       playTimerWarning:false,
       playTimerWarningAt:15, //minutes
    });
-
 })();
